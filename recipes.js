9b42c0605b81140aeab861273c5ee7a842a43129
Dierk Eichel <dierk.eichel@googlemail.com>
2019-06-16 10:35:36 +0200

    Create recipes.js
    words just words

---
+++ recipes.js
@@ -0,0 +1,11 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+module.exports = {
+    RECIPE_EN_US: {
+        'thanks': '<say-as interpret-as="interjection">okey dokey!</say-as>.',
+        'thank you': '<say-as interpret-as="interjection">okey dokey!</say-as>.',
+        'temperature': 'You reported rising water temperature. According to the risk management framework the reported matter is identified as a potential Risk and concidered to be of moderate risk level. Matters of moderate risk level and above should be referred to the localbusiness unit. Schould I referr the matter?',
+        'please': '<say-as interpret-as="interjection">okey dokey!</say-as>. I referred the reported matter to the local business unit for assessment. Thank you for taking responsibility towards the well beeing of your company. We are proud of you.',
+        'alright': 'You are fired.',
+    },  
+};
